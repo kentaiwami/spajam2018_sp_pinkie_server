1070f0655ade283ca82c477d419232956e0259d7
Kenta Iwami <g2117009@fun.ac.jp>
2018-05-12 22:28:30 +0900

    create pull model

--- model.py
+++ model.py
@@ -10,5 +10,15 @@ class User(db.Model):
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
+    salaries = db.relationship('Pull', backref='user', cascade='all, delete-orphan')
+
     def __repr__(self):
         return '{}'.format(self.name)
+
+
+class Pull(db.Model):
+    __tablename__ = 'pull'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
