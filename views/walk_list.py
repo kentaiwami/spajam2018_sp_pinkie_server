79b28ff97042c7f4e0f5b381800cfda607e8e78e
Kenta Iwami <g2117009@fun.ac.jp>
2018-05-13 04:27:28 +0900

    リストの詳細表示を実装

--- views/walk_list.py
+++ views/walk_list.py
@@ -29,3 +29,29 @@ def get():
 
 
     return jsonify({'results': results}), 200
+
+
+@app.route('/api/walk/list/<id>', methods=['GET'])
+def get_detail(id):
+    if request.args.get('user_id') is None:
+        return jsonify({'msg': 'user_idがない!!'}), 400
+
+    user = session.query(User).filter(User.id == request.args.get('user_id')).one_or_none()
+
+    if user is None:
+        return jsonify({'msg': 'User Not Found'}), 404
+
+    walk = session.query(Walk).filter(Walk.id == id).one_or_none()
+
+    if walk is None:
+        return jsonify({'msg': 'Walk Not Found'}), 404
+
+    sql = 'select from_unixtime(round(unix_timestamp(created_at) div(5 * 60)) *(5 * 60)) as timekey, count(*) from pull where user_id = %s group by timekey' % (user.id)
+    sql_res = session.execute(sql)
+
+    results = []
+
+    for res in sql_res:
+        results.append({'time': str(res[0]), 'count': res[1]})
+
+    return jsonify({'results': results}), 200
