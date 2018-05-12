681ecb88dcec2e9247842ccdd647e9512473a7e7
Kenta Iwami <g2117009@fun.ac.jp>
2018-05-12 22:46:29 +0900

    postでpullの作成を行うAPIを作成

--- views/pull.py
+++ views/pull.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request
 from model import User, Pull
 from database import session
+from jsonschema import validate, ValidationError
+
 
 app = Blueprint('pull_bp', __name__)
 
@@ -19,3 +21,29 @@ def get():
     session.close()
 
     return jsonify({'msg': 'Success'}), 200
+
+
+@app.route('/api/pull', methods=['POST'])
+def post():
+    schema = {'type': 'object',
+              'properties':
+                  {'id': {'type': 'integer', 'minimum': 0}},
+              'required': ['id']
+              }
+
+    try:
+        validate(request.json, schema)
+    except ValidationError as e:
+        return jsonify({'msg': e.message}), 400
+
+    user = session.query(User).filter(User.id == request.json['id']).one_or_none()
+
+    if user is None:
+        return jsonify({'msg': 'User Not Found'}), 404
+
+    pull = Pull(user_id=user.id)
+    session.add(pull)
+    session.commit()
+    session.close()
+
+    return jsonify({'id': request.json['id']}), 200
